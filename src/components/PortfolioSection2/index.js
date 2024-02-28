036665382e874d759e3c1c033b5eb131042f0ec7
Peter Nguyen <Petertrung@comcast.net>
2024-02-28 12:36:59 -0800

    add Pinyon font

--- src/components/PortfolioSection2/index.js
+++ src/components/PortfolioSection2/index.js
@@ -1,36 +1,36 @@
 import React from 'react'
 import SectionTitle from '../SectionTitle'
-import pImg1 from '../../images/portfolio/1.jpg'
-import pImg2 from '../../images/portfolio/2.jpg'
+//import pImg1 from '../../images/portfolio/1.jpg'
+//import pImg2 from '../../images/portfolio/2.jpg'
 import pImg3 from '../../images/portfolio/3.jpg'
-import pImg4 from '../../images/portfolio/4.jpg'
+//import pImg4 from '../../images/portfolio/4.jpg'
 import pImg5 from '../../images/portfolio/5.jpg'
 import pImg6 from '../../images/portfolio/6.jpg'
 import pImg7 from '../../images/portfolio/7.jpg'
 import pImg8 from '../../images/portfolio/8.jpg'
-import pImg9 from '../../images/portfolio/9.jpg'
+//import pImg9 from '../../images/portfolio/9.jpg'
 import ReactFancyBox from 'react-fancybox'
 import 'react-fancybox/lib/fancybox.css'
 
 
 const Portfolios = [
     {
-        Pimg:pImg1,
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144691&authkey=%21AAs6rsYvblmUf5Y&width=3648&height=2736',
     },
     {
-        Pimg:pImg2,
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144679&authkey=%21AB3g11m-zAmi1kc&width=3648&height=2736',
     },
     {
-        Pimg:pImg3,
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144683&authkey=%21AHfa_Fh4HM5wCzU&width=3648&height=2736',
     },
     {
-        Pimg:pImg4,
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144716&authkey=%21APje5jF7aBOJrl8&width=5184&height=2920',
+    },
+    {
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144601&authkey=%21AONqV0UUDfMwBYs&width=4640&height=2603',
     },
     {
         Pimg:pImg5,
-    },
-    {
-        Pimg:pImg6,
     },
     {
         Pimg:pImg7,
@@ -39,7 +39,16 @@ const Portfolios = [
         Pimg:pImg8,
     },
     {
-        Pimg:pImg9,
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144688&authkey=%21AHRGWDHEHsXsbtE&width=3648&height=2736',
+    },
+    {
+        Pimg:pImg6,
+    },
+    {
+        Pimg:'https://onedrive.live.com/embed?resid=8E2E516E19319715%2144605&authkey=%21ALR3tl8QrWFq6js&width=6960&height=3904',
+    },
+    {
+        Pimg:pImg3,
     },
 ] 
 
